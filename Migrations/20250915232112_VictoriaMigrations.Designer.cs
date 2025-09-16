ca5dab88cbe27ef4b418d87ba0081cd3d3fb864b
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2025-09-15 20:31:33 -0400

    se rename partidas a Victorias y se agrego empates y derrotas con su migracion

--- Migrations/20250915232112_VictoriaMigrations.Designer.cs
+++ Migrations/20250915232112_VictoriaMigrations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RegistroJugadores.DAL;
 
@@ -11,9 +12,11 @@ using RegistroJugadores.DAL;
 namespace Registro_Jugadores_TicTac1.Migrations
 {
     [DbContext(typeof(contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20250915232112_VictoriaMigrations")]
+    partial class VictoriaMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -29,12 +32,6 @@ namespace Registro_Jugadores_TicTac1.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("JugadorId"));
-
-                    b.Property<int>("Derrotas")
-                        .HasColumnType("int");
-
-                    b.Property<int>("Empates")
-                        .HasColumnType("int");
 
                     b.Property<string>("Nombres")
                         .IsRequired()
---
+++ Migrations/20250916002247_Derrotas.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Registro_Jugadores_TicTac1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Derrotas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Derrotas",
+                table: "Jugadores",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Empates",
+                table: "Jugadores",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Derrotas",
+                table: "Jugadores");
+
+            migrationBuilder.DropColumn(
+                name: "Empates",
+                table: "Jugadores");
+        }
+    }
+}
