6e49b444baf88cced983aae0422dd648340d5f60
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2025-09-18 22:50:26 -0400

    se completo el Tablero con sus comprobaciones para empate o victorias y los incrementos acordes (Falta actualizar tabla de jugadores y par de detalles esteticos)

--- Services/JuegosServices.cs
+++ Services/JuegosServices.cs
@@ -2,6 +2,7 @@
 using Registro_Jugadores_TicTac1.DAL;
 using Microsoft.EntityFrameworkCore;
 using RegistroJugadoresServices;
+using Microsoft.AspNetCore.Components;
 
 
 
@@ -10,15 +11,25 @@ namespace Registro_Jugadores_TicTac1.Services;
 
 
 
-public class JuegosServices()
+public class JuegosServices
 {
-    public  enum PlayerType { X,O};
-    private JugadoresServicios _jugadoresServicios { get; set; }=null!;
-    private PartidasServices _partidasServices { get; set; }=null!;
+    public enum PlayerType { X, O };
+    
+    private JugadoresServicios _jugadoresServicios { get; set; } = default!;
+    
+    private PartidasServices _partidasServices { get; set; } = default!;
     private PlayerType?[] board = new PlayerType?[9];
+
+    public JuegosServices(JugadoresServicios jugadoresServicios, PartidasServices partidasServices)
+    {
+        _jugadoresServicios = jugadoresServicios;
+        _partidasServices = partidasServices;
+        
+    }
+
     public PlayerType? CheckForWinner(PlayerType?[] board)
     {
-        
+
         var winningLines = new[]
         {
              new[] {0, 1, 2}, new[] {3, 4, 5}, new[] {6, 7, 8},// Horizontales
@@ -37,13 +48,19 @@ public class JuegosServices()
 
         return null; // No hay ganador
     }
-    public bool esEmpate(PlayerType?[] board) { 
-    // Comprobar empate
-      return  board.All(cell => cell != null);
-       }
+    public bool esEmpate(PlayerType?[] board) {
+        // Comprobar empate
+        return board.All(cell => cell != null);
+    }
 
-    public async Task GameOver(Partidas partida,Jugadores jugador1,Jugadores jugador2,PlayerType? ganador)
+    public async Task GameOver(Partidas partida, Jugadores jugador1, Jugadores jugador2, PlayerType? ganador)
     {
+        if(partida == null)
+        throw new ArgumentNullException(nameof(partida));
+
+        if (jugador1 == null || jugador2 == null)
+            throw new ArgumentNullException("Los jugadores no pueden ser null");
+
         partida.FechaFin = DateTime.Now;
         partida.EstadoPartida = "Finalizada";
         //estado de tablero
@@ -63,13 +80,19 @@ public class JuegosServices()
                 jugador2.Victorias++;
             }
         }
-        jugador1.Empates++;
-        jugador2.Empates++;
+        else
+        {
 
-        await _partidasServices.Modificar(partida);
+            jugador1.Empates++;
+            jugador2.Empates++;
+        }
 
-        if (jugador1 != null) await _jugadoresServicios.Modificar(jugador1);
-        if (jugador2 != null) await _jugadoresServicios.Modificar(jugador2);
+            await _partidasServices.Modificar(partida);
+
+            if (jugador1 != null) await _jugadoresServicios.Modificar(jugador1);
+            if (jugador2 != null) await _jugadoresServicios.Modificar(jugador2);
+        }
     }
-}
+
+
 
